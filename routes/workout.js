2c943880a5c68a1a17d1401abe680999b0f7b4a2
Sujala99 <121869443+Sujala99@users.noreply.github.com>
2024-10-28 15:40:16 +0545

    Update workout.js

--- routes/workout.js
+++ routes/workout.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const workoutController = require("../controller/Workout")
+const workoutController = require("../controller/workout")
 const authMiddleware = require("../auth"); // Import your authentication middleware
 
 
